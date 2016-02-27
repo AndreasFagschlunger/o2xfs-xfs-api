c836a4e81607845b1c82e0da85b4c1d11b4e1029
AndreasFagschlunger <github@fagschlunger.co.at>
2016-02-27 22:19:45 +0100

    Added XFS 3.10: CashUnit, CdmCaps, CdmStatus, PhysicalCashUnit

--- src/main/java/at/o2xfs/xfs/cdm/CdmGuidLights.java
+++ src/main/java/at/o2xfs/xfs/cdm/CdmGuidLights.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2016, Andreas Fagschlunger. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  *   - Redistributions of source code must retain the above copyright
  *     notice, this list of conditions and the following disclaimer.
- * 
+ *
  *   - Redistributions in binary form must reproduce the above copyright
  *     notice, this list of conditions and the following disclaimer in the
  *     documentation and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -29,7 +29,7 @@ package at.o2xfs.xfs.cdm;
 
 import at.o2xfs.xfs.XfsConstant;
 
-public enum GuidLights implements XfsConstant {
+public enum CdmGuidLights implements XfsConstant {
 
 	/*
 	 * @since v3.10
@@ -103,7 +103,7 @@ public enum GuidLights implements XfsConstant {
 
 	private final long value;
 
-	private GuidLights(final long value) {
+	private CdmGuidLights(final long value) {
 		this.value = value;
 	}
 
