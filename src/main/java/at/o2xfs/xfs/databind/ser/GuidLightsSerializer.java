835404799bcd80476c8325e28098275204cd5ce6
AndreasFagschlunger <github@fagschlunger.co.at>
2020-02-16 21:22:41 +0100

    GuidLights(Des|S)erializer, fix XfsEnumSet32Deserializer

--- src/main/java/at/o2xfs/xfs/databind/ser/GuidLightsSerializer.java
+++ src/main/java/at/o2xfs/xfs/databind/ser/GuidLightsSerializer.java
@@ -4,17 +4,36 @@ import java.util.List;
 import java.util.Set;
 
 import at.o2xfs.memory.core.MemoryGenerator;
+import at.o2xfs.memory.databind.BeanProperty;
 import at.o2xfs.memory.databind.MemorySerializer;
 import at.o2xfs.memory.databind.SerializerProvider;
 import at.o2xfs.xfs.XfsConstant;
+import at.o2xfs.xfs.databind.annotation.XfsGuidLights;
 
 public class GuidLightsSerializer<E extends Enum<E> & XfsConstant> extends MemorySerializer<List<Set<E>>> {
 
+	private final int length;
+
+	public GuidLightsSerializer() {
+		length = 0;
+	}
+
+	protected GuidLightsSerializer(int length) {
+		this.length = length;
+	}
+
+	@Override
+	public MemorySerializer<?> createContextual(SerializerProvider provider, BeanProperty prop) {
+		XfsGuidLights guidLights = prop.getMember().getAnnotation(XfsGuidLights.class);
+		return new GuidLightsSerializer<>(guidLights.length());
+	}
+
 	@Override
 	public void serialize(List<Set<E>> value, MemoryGenerator gen, SerializerProvider provider) {
-		XfsEnumSet32Serializer<E> serializer = new XfsEnumSet32Serializer<E>();
-		for (Set<E> each : value) {
-			serializer.serialize(each, gen, provider);
+		XfsEnumSet32Serializer<E> serializer = new XfsEnumSet32Serializer<>();
+		for (int i = 0; i < length; i++) {
+			Set<E> guidLights = value.get(i);
+			serializer.serialize(guidLights, gen, provider);
 		}
 	}
 }
